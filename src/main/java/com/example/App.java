2af8d492da5b541ee941266b099bc3051d8efda4
SURYA-CLG <surya.r2023c@vitstudent.ac.in>
2026-03-10 16:15:56 +0530

    Refactor App class and update test files for consistency

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -1,5 +1,7 @@
+package com.example;
+
 import java.util.Scanner;
-public class Main{
+public class App{
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter the marks: ");
--- src/test/java/com/example/AppTest.java
+++ src/test/java/com/example/AppTest.java
@@ -1,4 +1,4 @@
-package com.example
+package com.example;
 
 import org.junit.Test;
 import static org.junit.Assert.*;
