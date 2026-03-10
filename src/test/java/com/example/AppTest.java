c50b6e13ae2632a90fbe30dd0c78dbfcd0d08f6f
SURYA <160419610+suryaraja21@users.noreply.github.com>
2026-03-10 16:08:47 +0530

    Add unit test for App's grade method

--- src/test/java/com/example/AppTest.java
+++ src/test/java/com/example/AppTest.java
@@ -1,1 +1,12 @@
+package com.example
 
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class AppTest{
+  @Test
+  public void testGrade(){
+    App app = new App();
+    assertEquals('A',app.grade(90));
+  }
+}
